ef0d8c6316932b5caf98171dd4c03eeb7ca4848c
Shubhadeep Mandal <shubhadeepmandal394@gmail.com>
2020-07-29 22:17:13 +0530

    Update submit.php

--- website/submit.php
+++ website/submit.php
@@ -3,13 +3,13 @@
 $a=$_POST['first_name'];
 $b=$_POST['last_name'];
 $c=$_POST['email'];
-$d=$_POST['vech_name'];
+$d=$_POST['phone'];
 $e=$_POST['vech_num'];
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tamarillo_technologies";
+$dbname = "vehicle_authentication";
 
 //Creating Connection
 $con = new mysqli($servername,$username,$password,$dbname);
@@ -20,7 +20,8 @@ if($con->connect_error)
     die("Connection Failed !!" . $con->connect_error);
 }
 echo "Connected Successfully !!";
-$sql = "INSERT INTO users (fname,lname,mail,vech1,vech2) VALUES ('$a','$b','$c','$d','$e')";
+
+$sql = "INSERT INTO users (fname,lname,mail,phone,code) VALUES ('$a','$b','$c','$d','$e')";
 
 if($con->query($sql) === TRUE)
 {
